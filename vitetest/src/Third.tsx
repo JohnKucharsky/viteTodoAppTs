77af21cb58c986d4611d85cf86478d10519e1c26
John <vannok@live.com>
2022-07-04 10:14:49 +0300

    cmmt

---
+++ vitetest/src/Third.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+type productType = {
+  category: string;
+  price: string;
+  stocked: boolean;
+  name: string;
+};
+
+function FilterableProductTable({ products }: { products: productType[] }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={filterText}
+          placeholder="Search..."
+          onChange={(e) => setFilterText(e.target.value)}
+        />
+        <label>
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+            onChange={(e) => setInStockOnly(e.target.checked)}
+          />{" "}
+          Only show products in stock
+        </label>
+      </form>
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
+
+function ProductCategoryRow({ category }: { category: string }) {
+  return (
+    <tr>
+      <th colSpan={2}>{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }: { product: productType }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({
+  products,
+  filterText,
+  inStockOnly,
+}: {
+  products: productType[];
+  filterText: string;
+  inStockOnly: boolean;
+}) {
+  const rows: JSX.Element[] = [];
+  let lastCategory: string | null = null;
+
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />,
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+export default function Third() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
--- vitetest/src/main.tsx
+++ vitetest/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import Second from "./Second";
+import Third from "./Third";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -11,10 +12,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/second">Second</Link>
+        <Link to="/third">Third</Link>
       </div>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/second" element={<Second />} />
+        <Route path="/third" element={<Third />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
